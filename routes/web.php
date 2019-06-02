d093089f703a3c516b396b5c0fb7e2b719fd0b4a
Levanhieu <hieuu.dev99@gmail.com>
2019-06-02 22:49:33 +0700

    Check Admin

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::get('/admin', 'HomeController@admin')->middleware('CheckAdmin');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
