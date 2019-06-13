a283fbd003b8d586e376e18a016033c2af5b7461
Levanhieu <hieuu.dev99@gmail.com>
2019-06-14 00:51:23 +0700

    register, update teamplate login logout

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <div class="container">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+            <div class="col-xl-10 col-lg-12 col-md-9" style="margin-top: 170px;">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-password-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">{{ __('Reset Password') }}</h1>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    </div>
+                                    <form method="POST" action="{{ route('password.email') }}">
+                                        <div class="form-group">
+                                            <input type="email"
+                                                   class="form-control form-control-user @error('email') is-invalid @enderror"
+                                                   name="email" value="{{ old('email') }}" required autocomplete="email"
+                                                   autofocus id="exampleInputEmail" aria-describedby="emailHelp"
+                                                   placeholder="Enter Email Address...">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+                                            @enderror
+                                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
+
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            {{ __('Send Password Reset Link') }}
+                                        </button>
+                                    </form>
+
+                                </div>
                             </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
+
             </div>
+
         </div>
+
     </div>
-</div>
 @endsection
