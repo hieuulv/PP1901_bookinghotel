6c6d02294cf623b4264aaca850f5d6d6c663cc98
Levanhieu <hieuu.dev99@gmail.com>
2019-06-26 19:13:20 +0700

    New app login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,50 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ __('Laravel') }}</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-
-    <style>
-        .avatar {
-            border-radius: 50%;
-            -moz-border-radius: 50%;
-            -webkit-border-radius: 50%;
-            width: 210px;
-            height: 210px;
-        }
-        body {
-            background-color: blue ;
-        }
-    </style>
+    @include('layouts.app_login_register.top_asset')
 </head>
 <body>
 
 @yield('content')
 
 </body>
-<script>
-    // show image
-    $(document).ready(function () {
-
-        var readURL = function (input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-
-                reader.onload = function (e) {
-                    $('.avatar').attr('src', e.target.result);
-                };
-
-                reader.readAsDataURL(input.files[0]);
-            }
-        };
-
-        $(".file-upload").on('change', function () {
-            readURL(this);
-        });
-    });
-</script>
-
+    @include('layouts.app_login_register.bottom_asset')
 </html>
